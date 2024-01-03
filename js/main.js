67a24d66538e1604d4f152ced5a4757506556200
onochang <59571704+onochang@users.noreply.github.com>
2024-01-03 20:03:08 +0900

    js修正

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
-$(function () {
-  // drawer
-  $(".drawer").drawer();
+const open = document.getElementById("open");
+const overlay = document.getElementById("overlay");
+const drawer = document.getElementById("drawer");
+const close = document.getElementById("close");
+const menu = document.getElementsByClassName("drawer-menu-item");
 
+open.addEventListener("click", openDrawer);
+close.addEventListener("click", closeDrawer);
+overlay.addEventListener("click", closeDrawer);
+menu[0].addEventListener("click", closeDrawer);
+menu[1].addEventListener("click", closeDrawer);
+menu[2].addEventListener("click", closeDrawer);
+menu[3].addEventListener("click", closeDrawer);
+function openDrawer() {
+  overlay.classList.add("is-active");
+  drawer.classList.add("is-active");
+}
+
+function closeDrawer() {
+    overlay.classList.remove("is-active");
+    drawer.classList.remove("is-active");
+}
+
+
+$(function () {
   // slick
   $(".experience-list").slick({
     slidesToShow: 3,
